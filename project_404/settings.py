c5f5c8c018248d0be405679f8e77602da97a47e4
aaronskiba <71047780+aaronskiba@users.noreply.github.com>
2022-10-28 01:27:16 -0600

    Update settings.py

--- project_404/settings.py
+++ project_404/settings.py
@@ -146,7 +146,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
